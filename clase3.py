4f0c6ceae138af22c1f599c49d7d1c5a212a94d8
Yuli1079 <u20201186204@usco.edu.co>
2023-08-09 16:49:31 -0500

    Se actualizo la carpeta

---
+++ clase3.py
@@ -0,0 +1,15 @@
+print("Este programa se encarga de realizar las operaciones basicas entre dos numeros; por lo tanto digite dos valores: ")
+numero1 = float(input("Ingrese el primer numero: "))
+numero2= float(input("Ingrese el segundo numero: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2 
+multiplicacion = numero1 * numero2
+division = numero1/numero2
+Residuo= numero1%numero2 
+
+print("La suma de los digitos registrados es: ", suma)
+print("La resta de los digitos registrados es: ", resta)
+print("La multiplicacion de los digitos registrados es: ", multiplicacion)
+print("La division de los digitos registrados es: ", division)
+print("El residuo de la division de los digitos registrados es: ", Residuo)
