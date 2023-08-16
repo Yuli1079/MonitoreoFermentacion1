d495cd2e2f23cbf3eb53cc4c29a48130b3e62811
Yuli1079 <u20201186204@usco.edu.co>
2023-08-16 18:30:49 -0500

    Se agrego el codigo

--- clase_6/arreglos.py
+++ clase_6/arreglos.py
@@ -5,8 +5,23 @@ VarLista = ['hola', '1', '2', '3.5']
 varlista = ['Hector', True, 5.5, 8]
 print(varlista[3])
 
+#Elimina un elemento de la lista
+del varlista[0]
+
+#Agrega un elemento a la lista 
+varlista.insert(1,"cual")
+
+#Agrega unn elemento siempre al final
+varlista.append('USCO')
+
+#Agrega una variable que la persona quiera agregar
+varlista.append(input("Insete la variable para agrear a la lista"))
+
+
+
 #Imprimir una lista con ciclo for 
- 
+
+""" """ """
 for z in varlista:
     print(z)
 
@@ -18,3 +33,5 @@ while q < (len(varlista)):
 
     print(varlista[q])
     q +=1
+
+""" """ """
