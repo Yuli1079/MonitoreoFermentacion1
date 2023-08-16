8573543302fe90c1d91540b5f431d4024334fab6
Yuli1079 <u20201186204@usco.edu.co>
2023-08-16 18:28:32 -0500

    arreglos

---
+++ clase_6/arreglos.py
@@ -0,0 +1,20 @@
+Var = 1
+#Se crea un arreglo. 
+
+VarLista = ['hola', '1', '2', '3.5'] 
+varlista = ['Hector', True, 5.5, 8]
+print(varlista[3])
+
+#Imprimir una lista con ciclo for 
+ 
+for z in varlista:
+    print(z)
+
+#Imprimir lista con ciclo while 
+
+q = 0
+
+while q < (len(varlista)):
+
+    print(varlista[q])
+    q +=1
